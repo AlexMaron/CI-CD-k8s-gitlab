f631dca0936ca0437b7e68faea4ba73de3cb1265
vagrant <vagrant@k8s-master>
2021-08-03 13:04:55 +0000

    added vim config for k8s-master host, switch vagrant network to public 192.168.88.0

--- k8s-provision.sh
+++ k8s-provision.sh
@@ -2,12 +2,12 @@
 
 set -x
 
-if [[ ! -f /home/vagrant/.k8s-provision ]];then         
+if [[ ! -f /home/vagrant/.k8s-provision ]];then
   touch /home/vagrant/.k8s-provision
 
   apt-get update
   curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-  apt-get install -y python3-pip nodejs ruby-full neovim
+  apt-get install -y git python3-pip nodejs ruby-full mono-complete golang default-jdk ack-grep neovim
   gem install neovim
   curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
   python3 get-pip.py --force-reinstall
@@ -22,8 +22,18 @@ if [[ ! -f /home/vagrant/.k8s-provision ]];then
   echo 'alias vim=nvim' | tee -a /home/vagrant/.bashrc
   cp -r /vagrant/provisioning /home/vagrant/
   chmod o-w /home/vagrant/provisioning
-  cp -rf /vagrant/vim/.local /home/vagrant/
-  cp -rf /vagrant/vim/.config /home/vagrant/
-  if [[ ! -e /vagrant/ssh_key/id_rsa ]]; then ssh-keygen -b 2048 -t /vagrant/ssh_key/id_rsa -q -N ""; fi
+#  cp -rf /vagrant/vim/.local /home/vagrant/
+#  cp -rf /vagrant/vim/.config /home/vagrant/
+  if [[ ! -e /vagrant/ssh_key/id_rsa ]]; then ssh-keygen -b 2048 -t rsa -f /vagrant/ssh_key/id_rsa -q -N ""; fi
   chown -R vagrant:vagrant /home/vagrant
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim  
+  mkdir -p /home/vagrant/.config/nvim
+  if [[ -d /home/vagrant/.config/nvim ]];then
+    cp /vagrant/vim/.config/nvim/init.vim /home/vagrant/.config/nvim/
+  fi
+  vim +PluginInstall +qall
+  python3 ~/.vim/bundle/youcompleteme/install.py --go-completer
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+	         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  cd ~/.vim/bundle/vimproc.vim && make && cd -
 fi
